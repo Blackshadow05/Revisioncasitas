1d800305967d803d88f9314d8112e22337da3e0b
Esteban B <estebanbg055@gmail.com>
2025-06-11 19:28:23 -0600

    Add environment variables configuration

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   }
 }
 
