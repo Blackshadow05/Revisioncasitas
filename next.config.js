d3c04b2a101d2952dc7a75627db75dbee33914aa
Esteban B <estebanbg055@gmail.com>
2025-06-11 21:12:23 -0600

    Desactivar telemetría de Next.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,10 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
   },
-  output: 'standalone'
+  output: 'standalone',
+  telemetry: {
+    disable: true
+  }
 }
 
 module.exports = nextConfig 
